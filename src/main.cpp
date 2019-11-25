03502d069feeeb823cb1db29617e29ea35e0cd14
Jakob <jakob.koblinsky@gmail.com>
2019-11-25 15:36:02 -0500

    Rewrote entire program

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,87 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\me                                               */
+/*    Created:      Fri Nov 22 2019                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// Claw                 motor         1               
+// Drivetrain           drivetrain    20, 11          
+// Shaft_Right          motor         2               
+// Shaft_Left           motor         10              
+// CenterWheel          motor         16              
+// Vision7              vision        7               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
 #include "vex.h"
-#include "VisionSensor.h"
-#include "main.h"
-
 
 using namespace vex;
 
-motor        LeftMotor(PORT1, gearSetting::ratio18_1, false);
-motor        RightMotor(PORT2, gearSetting::ratio18_1, true);
-motor        SideMotor(PORT3, gearSetting::ratio18_1, false);
-motor        LiftLMotor(PORT4, gearSetting::ratio36_1, true);
-motor        LiftRMotor(PORT5, gearSetting::ratio36_1, false);
-//drivetrain   Drivetrain = drivetrain(LeftMotor, RightMotor, 319, 295, distanceUnits::mm);
-controller   Controller1;
-
-using namespace vex;
-
-double liftPos = 0;
+void selfControl();
 
 int main() {
-  //Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
+  // Initializing Robot Configuration. DO NOT REMOVE!
 
-  LiftLMotor.setStopping(brakeType::hold);
-  LiftRMotor.setStopping(brakeType::hold);
+  vexcodeInit(); 
+  selfControl();
+}
 
-  while(true) {        
-        LeftMotor.spin(directionType::fwd, Controller1.Axis3.value() + Controller1.Axis1.value(), velocityUnits::rpm);
-        RightMotor.spin(directionType::fwd, Controller1.Axis3.value() - Controller1.Axis1.value(), velocityUnits::rpm);
-        SideMotor.spin(directionType::fwd, Controller1.Axis4.value(), velocityUnits::rpm);
-        //LiftLMotor.rotateTo(liftPos, rotationUnits::deg, false);
-        //LiftRMotor.rotateTo(liftPos, rotationUnits::deg, false);
+void selfControl(){
 
-        if (Controller1.ButtonUp.pressing()) {
-            LiftLMotor.spin(directionType::fwd, 50, velocityUnits::pct);
-            LiftRMotor.spin(directionType::fwd, 50, velocityUnits::pct);
-        }
-        else if (Controller1.ButtonDown.pressing()) {
-            LiftLMotor.spin(directionType::rev, 25, velocityUnits::pct);
-            LiftRMotor.spin(directionType::rev, 25, velocityUnits::pct);
-        }
-        else {
-            LiftLMotor.stop();
-            LiftRMotor.stop();
-        }
+  Shaft_Left.setStopping(hold);
+  Shaft_Right.setStopping(hold);
+  Claw.setStopping(hold);
 
-        VisionSensor.takeSnapshot(GREEN_CUBE);
-        if (VisionSensor.largestObject.exists && VisionSensor.largestObject.width > 50) {
-            Brain.Screen.setPenColor(vex::color::white);
-            Brain.Screen.setFillColor(vex::color::green);
-            Brain.Screen.drawRectangle(0,0, 480, 240);
-        }
-        else {
-            VisionSensor.takeSnapshot(ORANGE_CUBE);
-            if (VisionSensor.largestObject.exists && VisionSensor.largestObject.width > 50) {
-                Brain.Screen.setPenColor(vex::color::white);
-                Brain.Screen.setFillColor(vex::color::orange);
-                Brain.Screen.drawRectangle(0,0, 480, 240);
-            }
-            else {
-                VisionSensor.takeSnapshot(PURPLE_CUBE);
-                if (VisionSensor.largestObject.exists && VisionSensor.largestObject.width > 50) {
-                    Brain.Screen.setPenColor(vex::color::white);
-                    Brain.Screen.setFillColor(vex::color::purple);
-                    Brain.Screen.drawRectangle(0,0, 480, 240);
-                }
-                else {
-                    Brain.Screen.clearScreen();
-                }
-            }
-        }
+  int shaftSpeed = 50;
+  int clawSpeed = 50;
+  int driveTrainSpeed = 50;
+  int preciseSub = 40;
 
-        task::sleep(100);
+  int p = 0;
+
+  while (true){
+
+    // Percision hold
+    if (Controller1.ButtonA.pressing()){
+      p = preciseSub;
     }
+    else{
+      p = 0;
+    }
+
+    // Shaft control
+    if (Controller1.ButtonUp.pressing()){
+      Shaft_Left.spin(forward, shaftSpeed - p, percent);
+      Shaft_Right.spin(forward, shaftSpeed - p, percent);
+    }
+    else if (Controller1.ButtonDown.pressing()){
+      Shaft_Left.spin(reverse, shaftSpeed - p, percent);
+      Shaft_Right.spin(reverse, shaftSpeed - p, percent);
+    }
+    else {
+      Shaft_Left.stop();
+      Shaft_Right.stop();
+    }
+
+    // Claw control
+    if (Controller1.ButtonL2.pressing()){
+      Claw.spin(forward, clawSpeed - p, percent);
+    }
+    else if (Controller1.ButtonR2.pressing()){
+      Claw.spin(reverse, clawSpeed - p, percent);
+    }
+    else {
+      Claw.stop();
+    }
+
+    // Center wheel control
+    CenterWheel.spin(forward, Controller1.Axis4.position(percent), percent);
+
+  }
 }
